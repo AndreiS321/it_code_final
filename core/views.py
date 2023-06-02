6c13faf42261aae4582ae7305b70901435b46a3c
Andrei <skibin-03@mail.ru>
2023-06-02 16:06:21 +0500

    Added class based view for index page in core app

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+
+from profiles.mixins import AuthMenuMixin
 
 
-def index(request):
-    return render(request, "core/index.html")
+class IndexView(AuthMenuMixin, View):
+    template_name = "core/index.html"
+
+    def get(self, request):
+        context = self.get_user_context_data()
+        return render(request, self.template_name, context=context)
