7295e85c95e94abd922ac6af34b227e88302939e
Andrei <skibin-03@mail.ru>
2023-06-07 14:10:48 +0500

    Added new views (password changing, profile removal) and templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import logout
-from django.contrib.auth.views import LoginView as LoginViewDjango
+from django.contrib.auth.views import LoginView as LoginViewDjango, \
+    PasswordChangeView
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views import View
-from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
 import utils
 from forum import models as forum_models
@@ -33,11 +34,53 @@ class ProfileUpdate(AuthMenuMixin, LoginRequiredMixin,
     template_name = "profiles/update.html"
     extra_context = {"title": "Редактирование профиля"}
 
-    def get(self, request, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
         if not utils.is_same_user(request.user, obj.pk):
             raise PermissionDenied()
-        return super().get(request, args, kwargs)
+        return super().dispatch(request, args=args, kwargs=kwargs)
+
+    def get_success_url(self):
+        return reverse("profiles:profile",
+                       kwargs={"pk": self.request.user.pk})
+
+
+class ProfileDelete(AuthMenuMixin, LoginRequiredMixin,
+                    DeleteView):
+    model = models.Profile
+    template_name = "profiles/delete.html"
+    extra_context = {"title": "Удаление профиля"}
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if not utils.is_same_user(request.user, obj.pk):
+            raise PermissionDenied()
+        return super().dispatch(request, args=args, kwargs=kwargs)
+
+    def get_success_url(self):
+        return reverse("core:index")
+
+
+class ChangePasswordView(AuthMenuMixin, LoginRequiredMixin,
+                         PasswordChangeView):
+    extra_context = {"title": "Смена пароля"}
+    template_name = "profiles/change_password.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if not utils.is_same_user(request.user, obj.pk):
+            raise PermissionDenied()
+        self.object = obj
+        return super().dispatch(request, args=args, kwargs=kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(kwargs=kwargs)
+        context["object"] = self.object
+        return context
+
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        return get_object_or_404(models.Profile, pk=pk)
 
     def get_success_url(self):
         return reverse("profiles:profile",
@@ -55,7 +98,6 @@ class RegisterView(AuthMenuMixin, CreateView):
 class LoginView(AuthMenuMixin, LoginViewDjango):
     template_name = "profiles/login.html"
     extra_context = {"title": "Вход"}
-
 
     def get_success_url(self):
         return reverse("core:index")
